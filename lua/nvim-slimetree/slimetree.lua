27d35eba7f1530b58da45238921bb537e16b658e
James Conigrave <james.conigrave@gmail.com>
2024-10-28 09:44:18 +1100

    add minus_metadata to skip nodes

--- lua/nvim-slimetree/slimetree.lua
+++ lua/nvim-slimetree/slimetree.lua
@@ -41,6 +41,7 @@ local skip_nodes = {
     ["fenced_code_block"] = true,
     ["fenced_code_block_delimiter"] = true,
     ["inline"] = true,
+    ["minus_metadata"] = true,
     ["atx_h1_marker"] = true,
     ["atx_h2_marker"] = true,
     ["atx_h3_marker"] = true,
